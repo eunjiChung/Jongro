7411a21c053e5c975a3ac6a9751586a268c6e034
LeeHanEOl <slowgoaway@hanmail.net>
2017-01-22 17:03:01 +0900

    Add files via upload

---
+++ haneol/전공공부/백준/[1월 22일] 만취한 상범.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	
+	int testcase, n, i, j, k, count = 0;
+	int str[100] = { 0 };  // 0¿Ã∏È ¥›»˘, 1¿Ã∏È ø≠∏≤
+	scanf_s("%d", &testcase);
+
+	for (i = 0; i<testcase; i++) {
+		count = 0;
+		scanf_s("%d", &n);		
+		memset(str, 0, sizeof(str));
+
+		for (j = 0; j < n; j++) {
+			str[j] = 1;
+			if ((j % 2) != 0) {
+				str[j] = 0;
+			}
+		}
+
+		for (j = 3; j <= n; j++) {
+			for (k=j-1; k < n  ; k += j) {
+				if (str[k] == 0) {
+					str[k] = 1;
+				}
+				else {
+					str[k] = 0;
+				}
+			}
+			
+		}
+
+
+		for (j = 0; j < n; j++)
+			if (str[j] == 1)
+				count += 1;
+
+		printf("\n count = %d\n", count);
+		
+		
+	}
+
+	scanf_s("%d");
+
+	return 0;
+}
